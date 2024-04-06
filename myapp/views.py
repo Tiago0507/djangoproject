7948615f4c1e93b4ffce85d92d6ce3d0783c89cd
TiagoVG <svalenciagarcia707@gmail.com>
2024-04-06 13:32:01 -0500

    Se sube lo primero visto sobre render y plantillas para renderizar htmls

--- myapp/views.py
+++ myapp/views.py
@@ -11,7 +11,7 @@ from django.shortcuts import get_object_or_404
 
 #Nueva función que será la primera cuando se visite el servidor
 def index(request):
-    return HttpResponse('Index page')
+    return render(request, 'index.html')
 
 #Ahora la función hello requiere dos parámetros, así que para poderla ejecutar tenemos que pasar otro string.
 def hello(request, username):
@@ -19,18 +19,18 @@ def hello(request, username):
     return HttpResponse("<h2>Hello %s</h2>" % username)
 
 def about(request):
-    return HttpResponse("About")
+    return render(request, 'about.html')
 
 def projects(request):
     #Para usar el Json primero se debe convertir los projects en una lista de python
     projects = list(Project.objects.values())
     #Al usar Json response, ya no necesitamos usar un String, sino que podemos directamente pasarle la lista de los 
     #objetos Project
-    return JsonResponse(projects, safe=False)
+    return render(request, 'projects.html')
 
 #Ahora tasks tendrá como otro param el id
 def tasks(request, id):
     #Se guarda una tarea con el id especificado
     task = get_object_or_404(Task, id=id)
     #Se usa % para concatenar el titulo de la tarea
-    return HttpResponse('task: %s' % task.title)
+    return render(request, 'tasks.html')
